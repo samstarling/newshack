1ffcf8bf5d50f1d9dc42c63db22ea5460653dded
Leyths <leyths@gmail.com>
2013-10-17 14:47:14 +0100

    More contentapi jazz

--- backend/lib/model/contentapi.rb
+++ backend/lib/model/contentapi.rb
@@ -1,15 +1,32 @@
-require 'summarize'
 require 'nokogiri'
 
 class ContentApi
 
     def initialize data
-    @body = data['body']
+        @body = data['body']
+        @summary = data['summary']
+        @created = data['lastUpdated']
+        @imagehref = data['media']['images']['index'].first[1]['href']
     end
 
     def as_hash
     {
-        body: @body
+        body: self.body(),
+        summary: @summary,
+        created: @created,
+        image: @imagehref
     }
+    end
+
+    def body
+        body = Nokogiri::XML(@body)
+        paragraphs = body.xpath("//xmlns:paragraph")
+        summary = ""
+        paragraphs.each do |pg| 
+            if pg.attribute('role') == nil
+                summary = summary + pg.content + "\n"
+            end
+        end
+        return summary
     end
 end
