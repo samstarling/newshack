685933b9f6b9b844b90f410bba6d95357540c0da
Sam Starling <sam.starling@gmail.com>
2013-10-17 12:57:29 +0100

    Updated gemfile.

--- backend/lib/model/storyline.rb
+++ backend/lib/model/storyline.rb
@@ -2,12 +2,14 @@ class Storyline
   def initialize data
     @title = data["preferredLabel"]
     @dateCreated = data["dateCreated"]
+    @slots = data["hasSlot"]["@set"].map { |slot| slot["@id"] }
   end
   
   def as_hash
     {
       title: @title,
-      timestamp: @dateCreated
+      timestamp: @dateCreated,
+      slots: @slots
     }
   end
 end
