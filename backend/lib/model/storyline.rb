090258ffdfb4ceee7dcd2f17aff1d1c607e1b339
Sam Starling <sam.starling@gmail.com>
2013-10-17 13:48:27 +0100

    Now pulls out the titles of events

--- backend/lib/model/storyline.rb
+++ backend/lib/model/storyline.rb
@@ -1,12 +1,14 @@
 require 'client/juicer_client'
+require 'model/event'
 
 class Storyline
   def initialize data
     @title = data.first["preferredLabel"]
     @dateCreated = data.first["dateCreated"]
     @slots = data.first["hasSlot"]["@set"].map do |uri|
-      slot_id = data.select { |item| item["@id"] == uri["@id"] }.first["contains"]["@id"]
-      slot_id
+      event_uri = data.select { |item| item["@id"] == uri["@id"] }.first["contains"]["@id"]
+      event_data = data.select { |item| item["@id"] == event_uri }.first
+      Event.new(event_data).as_hash
     end
     enrich_slots
   end
