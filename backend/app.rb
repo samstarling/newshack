eb9ed8aaef2f4d598e6f46ff8569c59a47bc1770
Sam Starling <sam.starling@gmail.com>
2013-10-17 12:15:02 +0100

    Fixing backend app

--- backend/app.rb
+++ backend/app.rb
@@ -5,10 +5,5 @@ $LOAD_PATH.push File.expand_path("../lib", __FILE__)
 require 'sinatra'
 
 get '/' do
-  @title = 'Storylines' 
-  erb :index
-end
-
-get '/test/unit' do
-  erb :jasmine, :layout => false
+  "This is the backend app"
 end
